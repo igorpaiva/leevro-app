6e6e696e80e2614c1c833cb85a1a45c747a28dcc
igor.araujo <igor.araujo@ee.ufcg.edu.br>
2023-05-21 17:27:55 -0300

    [infra] - update angular material to v15

--- leevro-frontend/src/app/app.module.ts
+++ leevro-frontend/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
